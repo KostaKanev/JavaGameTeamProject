779f7882ff0e6e0eb8ad138923249fb9d68830dd
Kosta Kanev <kosta.kanev@gmail.com>
2015-10-29 14:50:23 +0200

    Add track

---
+++ GameTeamProject/src/game/Track.java
@@ -0,0 +1,35 @@
+package game;
+
+import graphics.ImgLoader;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Track {
+
+    public int x;
+    public int y;
+    public int velosity;
+
+    private BufferedImage trackImage;
+
+    public Track (int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        this.velosity = 2;
+
+        this.trackImage = ImgLoader.loadImage("/img/bkg.jpg");
+    }
+
+    public void tick(){
+
+        this.y += 1;
+    }
+
+    public void render(Graphics g){
+
+        g.drawImage(this.trackImage,0 , this.y, null);
+    }
+
+}
