04b61f90f4de5dbba019a7afdf0e086f22aad112
IskraNikolova <air_iskra@yahoo.com>
2015-10-29 14:04:36 +0200

    now added

---
+++ GameTeamProject/src/game/InputHandler.java
@@ -0,0 +1,43 @@
+package game;
+
+import display.Display;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputHandler implements KeyListener{
+
+    public InputHandler(Display display){
+        display.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_LEFT){
+            Player.moovingLeft = true;
+
+        }
+        if(keyCode == KeyEvent.VK_RIGHT){
+            Player.moovingRight = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_LEFT){
+            Player.moovingLeft = false;
+        }
+        if(keyCode == KeyEvent.VK_RIGHT){
+            Player.moovingRight = false;
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+}
